f432d96bccb3f1c7e880acc4374aea7e538fc5e7
clebersandbox <clebersandbox@gmail.com>
2021-09-15 05:59:23 -0300

    Aula #06

--- src/pages/Promotion/Search/Search.js
+++ src/pages/Promotion/Search/Search.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import PromotionCard from '../../../components/Promotion/Card/Card';
 
 const PagesPromotionSearch = () => {
-  return <div>Promotion Search Page</div>;
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/promotions?_embed=comments').then(res => {
+      setPromotions(res.data);
+    });
+  }, []);
+
+  return (
+    <div
+      style={{
+        maxWidth: 800,
+        margin: '30px auto'
+      }}
+    >
+      {promotions.map(promo => (
+        <PromotionCard promotion={promo} />
+      ))}
+    </div>
+  );
 };
 
 export default PagesPromotionSearch;
